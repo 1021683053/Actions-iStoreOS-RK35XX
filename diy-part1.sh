1d4ee8163d58830203c9d06cb16c5e15bf502bb4
Liweifeng <1021683053@qq.com>
2025-01-06 15:36:16 +0800

    fix: update

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,7 @@ sed -i "s/OPENWRT_RELEASE.*/OPENWRT_RELEASE=\"%D %V ${date_version} by ${author}
 
 # 拉取我的软件包仓库
 sed -i '$a src-git xmpackage https://github.com/xiaomeng9597/openwrt-packages2.git;main' feeds.conf.default
+
+# 拉取kenzok8的软件包仓库
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
